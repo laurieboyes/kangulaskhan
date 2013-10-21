35e5123fbb5af87aef66986bf9ae529695e82e99
Lrnk <laurie.boyes@gmail.com>
2013-10-21 21:01:47 +0100

    Rendering a datamodel in a table
    In this case some Pokémon

---
+++ public/js/controllers/PokeCtrl.js
@@ -0,0 +1,9 @@
+
+
+function PokeCtrl($scope) {
+    $scope.pokemonList = [
+        {number: '001', name: 'Bulbasaur', nickname: 'Georgia', level:10},
+        {number: '004', name: 'Charmander', nickname: 'Charles', level:16}
+    ];
+
+}
