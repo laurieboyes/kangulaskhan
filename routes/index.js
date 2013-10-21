d8ef0f44ff4c17fcc6b7fe451ae55d389f9baa06
Lrnk <laurie.boyes@gmail.com>
2013-10-21 19:34:04 +0100

    Added app as it stands to nav

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 
 /*
- * GET home page.
+ * GET all the pages.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Kangulaskhan | Look at my cool list' });
+};
+
+exports.introductions = function(req, res){
+    res.render('introductions', { title: 'Kangulaskhan | Who are you?' });
 };
